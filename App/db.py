4bf49d7ef03346f720f9208c8dd269228509ad8c
Alexanderkabui <alexanderkabua@gmail.com>
2021-01-02 20:45:36 +0300

    add links model

---
+++ App/db.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+from App import uuid, db
+
+
+class Links(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(256), unique=True, nullable=False)
+    title = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    link = db.Column(db.String(128), nullable=False)
