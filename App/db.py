472348c4de4bbb43718a1541915448656169a54f
Alexanderkabui <alexanderkabua@gmail.com>
2021-01-02 20:48:45 +0300

    add control for links

--- App/db.py
+++ App/db.py
@@ -5,6 +5,7 @@ from App import uuid, db
 class Links(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     public_id = db.Column(db.String(256), unique=True, nullable=False)
+    access = db.Column(db.String(20),default="private")
     title = db.Column(db.String(128), nullable=False)
     description = db.Column(db.Text, nullable=True)
     link = db.Column(db.String(128), nullable=False)
