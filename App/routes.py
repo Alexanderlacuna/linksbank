1be753a0e6c98585d83156dc24871c5cb6d78033
Alexanderkabui <alexanderkabua@gmail.com>
2021-01-03 19:47:41 +0300

    add api routes for link

--- App/routes.py
+++ App/routes.py
@@ -1,6 +1,19 @@
 from . import app, jsonify
 
+@app.route("/<id>",methods=["GET"])
+def get_link(id):
+	# make call  to  the datab
+	raise NotImplementedError
 
-@app.route("/")
-def runs():
-    return jsonify({"hello": "links"})
+@app.route("/save",methods =["POST"])
+def save_link():
+	raise NotImplementedError
+
+
+@app.route("/update",methods=["POST"])
+def update_link():
+	raise NotImplementedError
+
+@app.route("/delete",methods=["DELETE"])
+def delete_link():
+	raise NotImplementedError
