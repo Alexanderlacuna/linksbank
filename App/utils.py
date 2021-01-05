7c141443cabc4255cd24909bd42abf14dc2bfd7e
Alexanderkabui <alexanderkabua@gmail.com>
2021-01-05 22:33:27 +0300

    add token checker for authentication

--- App/utils.py
+++ App/utils.py
@@ -1,2 +1,15 @@
-from functools import wrap
+from functools import wraps
+def token_required(func):
+	@wraps
+	def inner_function(*args,**kwargs):
+		token = request.args.get("token")
+		if token is None:
+			return jsonify({"message":"Error occurred"}),401
 
+		try:
+			# need to fetch secret key
+			current_user = jwt.decode(token,"secret")
+
+		except Exception as e:
+			return jsonify({"message":"authentication failed"})
+		return func(current_user,*args,**kwargs)
