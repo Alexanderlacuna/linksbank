395e7151ea180b7007afe18ab98624d07080b459
Alexanderkabui <alexanderkabua@gmail.com>
2021-01-05 22:38:34 +0300

    pep8 formatting
    add  generate_jwt_token

--- App/utils.py
+++ App/utils.py
@@ -1,15 +1,25 @@
 from functools import wraps
+
+
+def generate_jwt_token():
+    pass
+
+
+def validate_token():
+    pass
+
+
 def token_required(func):
-	@wraps
-	def inner_function(*args,**kwargs):
-		token = request.args.get("token")
-		if token is None:
-			return jsonify({"message":"Error occurred"}),401
+    @wraps
+    def inner_function(*args, **kwargs):
+        token = request.args.get("token")
+        if token is None:
+            return jsonify({"message": "Error occurred"}), 401
 
-		try:
-			# need to fetch secret key
-			current_user = jwt.decode(token,"secret")
+        try:
+            # need to fetch secret key
+            current_user = jwt.decode(token, "secret")
 
-		except Exception as e:
-			return jsonify({"message":"authentication failed"})
-		return func(current_user,*args,**kwargs)
+        except Exception as e:
+            return jsonify({"message": "authentication failed"})
+        return func(current_user, *args, **kwargs)
